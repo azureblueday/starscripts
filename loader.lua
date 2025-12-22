281a647f359b9a70f40722f6f7920821fec1b6c0
azureblueday <gamerspace32@gmail.com>
2025-12-22 14:31:45 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -16,6 +16,7 @@ local Scripts = {
 	[3150475059] = "https://api.luarmor.net/files/v3/loaders/317ec9710555a4bbf0389a4f2c503fae.lua", -- ff2
 	[184199275] = "https://api.luarmor.net/files/v3/loaders/bc4438f2488e366d2848d39ddb70cacc.lua", -- UF
 	[4931927012] = "https://api.luarmor.net/files/v3/loaders/40ef3f5eb3cede5e90381a450c3a6e40.lua", -- BL
+	[8558141897] = "https://api.luarmor.net/files/v3/loaders/12dd49556b1b655239cedce85454bdcd.lua", -- BL
 }
 
 local scriptUrl = Scripts[game.GameId]
