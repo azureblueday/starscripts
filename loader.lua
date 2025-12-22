145c687f3f93113131bf0c8e6ad7688ab6b6a1b8
azureblueday <gamerspace32@gmail.com>
2025-12-22 08:58:00 -0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,29 +1,50 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
 
-local GameID = game.GameId
-local Exploit = identifyexecutor()
+--------------------------------------------------
+-- Executor check (blacklist)
+--------------------------------------------------
+if typeof(identifyexecutor) ~= "function" then
+	player:Kick("Unsupported executor.")
+	return
+end
 
-local Player = game:GetService("Players").LocalPlayer
-local LogService = game:GetService("LogService")
-local ScriptContext = game:GetService("ScriptContext")
+local executorName = identifyexecutor()
 
--- hi skidders!!!
-local Shitsploits = { "Solara", "Xeno" }
-
-local GameIDs = {
-    ["3150475059"] = "https://api.luarmor.net/files/v3/loaders/317ec9710555a4bbf0389a4f2c503fae.lua", -- FF2
-    ["184199275"] = "https://api.luarmor.net/files/v3/loaders/bc4438f2488e366d2848d39ddb70cacc.lua", -- UF
-    ["4931927012"] = "https://api.luarmor.net/files/v3/loaders/40ef3f5eb3cede5e90381a450c3a6e40.lua", -- BL
+-- Executors you want to BAN
+local BannedExecutors = {
+	["Xeno"] = true,
+	["Solara"] = true
 }
 
-local function Execute(IDs)
-    return loadstring(game:HttpGet(IDs[tostring(GameID)]))()
+if BannedExecutors[executorName] then
+	player:Kick("Nova | Unsupported Executor: " .. executorName)
+	return
 end
 
--- // Key detection (more detailed kick message because LRM's is not that great)
-if not script_key and not getgenv().script_key then
-    return Player:Kick("Nova | Please make sure you include the script_key part ABOVE the loadstring, otherwise Luarmor will not be able to recognize that you bought.")
+--------------------------------------------------
+-- GameId check
+--------------------------------------------------
+local Scripts = {
+	[3150475059] = "https://api.luarmor.net/files/v3/loaders/317ec9710555a4bbf0389a4f2c503fae.lua", -- ff2
+	[184199275] = "https://api.luarmor.net/files/v3/loaders/bc4438f2488e366d2848d39ddb70cacc.lua", -- UF
+	[4931927012] = "https://api.luarmor.net/files/v3/loaders/40ef3f5eb3cede5e90381a450c3a6e40.lua", -- BL
+}
+
+local scriptUrl = Scripts[game.GameId]
+
+if not scriptUrl then
+	player:Kick("This game is not supported.")
+	return
 end
 
-if table.find(Shitsploits, Exploit) then
-    return Player:Kick("Nova | Executor is not supported by Nova.")
+--------------------------------------------------
+-- Load script
+--------------------------------------------------
+local success, err = pcall(function()
+	loadstring(game:HttpGet(scriptUrl))()
+end)
+
+if not success then
+	player:Kick("Failed to load script. Rejoin.")
 end
