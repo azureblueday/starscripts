ec6e27148acd9325d0431cb062d7e0ce2cc4fe2e
azureblueday <gamerspace32@gmail.com>
2026-01-16 22:02:36 -0800

    Update and rename lite.lua to loaderv2.lua

---
+++ loaderv2.lua
@@ -0,0 +1,20 @@
+if game.PlaceId == 8204899140 then
+	loadstring(game:HttpGet("https://cdn.snc.dev/695c6cf7c19b7064b9248279/0rctdgjr97qc"))()
+	wait(0.1)
+	loadstring(game:HttpGet("https://cdn.snc.dev/696b1914700811ec3fbf07be/9c09mngppbt"))()
+end
+
+if game.PlaceId == 104709320604721 then
+	loadstring(game:HttpGet("https://cdn.snc.dev/695c6cf7c19b7064b9248279/0rctdgjr97qc"))()
+	wait(0.1)
+	loadstring(game:HttpGet("https://cdn.snc.dev/696b1914700811ec3fbf07be/9c09mngppbt"))()
+end
+
+if game.PlaceId == 8206123457 then
+	loadstring(game:HttpGet("https://cdn.snc.dev/696b1914700811ec3fbf07be/9c09mngppbt"))()
+end
+
+if not scriptUrl then
+	player:Kick("Nova | This game is not supported.")
+	return
+end
