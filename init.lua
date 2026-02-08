c956202a5065302ff32d98ea96461a058bd841cb
azureblueday <gamerspace32@gmail.com>
2026-02-08 14:55:57 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 local ProjectId = "698910339dab15797321e6e7"
 local Scripts = {
     [4864117649] = "https://cdn.snc.dev/698910339dab15797321e6e7/8s3ia71wr35", -- utg
+    [6331902150] = "https://api.jnkie.com/api/v1/luascripts/public/bc4bd8e1a27fcd9ead2c8986327c15632f180206d2579c6c198ef09649546772/download", -- forsaken
+    [8558141897] = "https://api.jnkie.com/api/v1/luascripts/public/211262fe32dfcb5d25a2a3afc47d95f27226ebed0ba5795a1abbe3ffc4bd8ad3/download", -- flag fb
 }
 
 local Script = Scripts[game.GameId]
@@ -9,3 +11,5 @@ if Script then
 else
    game:GetService("Players").LocalPlayer:Kick("Script is currently unavaliable for this game!")
 end
+
+print("loader")
