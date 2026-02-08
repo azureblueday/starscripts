2a05d32074891d77d06b5962633508fae276ebe5
azureblueday <gamerspace32@gmail.com>
2026-02-08 13:32:50 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-print("loader v0.5")
+print("loader v0.6")
 local Scripts = {
     [4864117649] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle5rbyo0001ysz7c4juqu48",
     [8558141897] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle6k4o00001q4oo5zuaipaa",
+    [6331902150] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle9do8k0001tp4o59y0zfz3",
 }
 local Script = Scripts[game.GameId]
 if Script then
