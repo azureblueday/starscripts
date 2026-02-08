1a82258d18e3b350e528080e34c78b9f3c3df2c8
azureblueday <gamerspace32@gmail.com>
2026-02-08 15:12:53 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 local ProjectId = "698910339dab15797321e6e7"
+print("loader v0.9")
 local Scripts = {
     [4864117649] = "https://cdn.snc.dev/698910339dab15797321e6e7/8s3ia71wr35", -- utg
     [6331902150] = "https://raw.githubusercontent.com/azureblueday/starscripts/refs/heads/main/forsaken-obfuscated.lua", -- forsaken
@@ -11,5 +12,3 @@ if Script then
 else
    game:GetService("Players").LocalPlayer:Kick("Script is currently unavaliable for this game!")
 end
-
-print("loader v0.8")
