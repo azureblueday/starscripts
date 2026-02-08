ef864496f4a19feb75aa118d3b914c614b67e3cd
azureblueday <gamerspace32@gmail.com>
2026-02-08 14:27:34 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
-print("loader v0.6")
+print("loader v0.7")
 local Scripts = {
-    [4864117649] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle5rbyo0001ysz7c4juqu48",
-    [8558141897] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle6k4o00001q4oo5zuaipaa",
-    [6331902150] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmle9do8k0001tp4o59y0zfz3",
+    [4864117649] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebb01m0001ffa5g5590gfg", -- utg
+    [8558141897] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebcr4t0005ffa5lxdsbfga", -- flag fb
+    [6331902150] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebc6wa0003ffa57d8vnhw4", -- forsaken
 }
 local Script = Scripts[game.GameId]
 if Script then
