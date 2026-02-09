53a7d657f3a3e49c461c8c723764bbb73a9b9be6
azureblueday <gamerspace32@gmail.com>
2026-02-08 20:53:45 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 local ProjectId = "698910339dab15797321e6e7"
-print("loader v0.9.1")
+print("loader v0.9.2")
 local Scripts = {
-    [4864117649] = "https://cdn.snc.dev/698910339dab15797321e6e7/8s3ia71wr35", -- utg
-    [6331902150] = "https://raw.githubusercontent.com/azureblueday/starscripts/refs/heads/main/forsaken-obfuscated.lua", -- forsaken
-    [8558141897] = "https://raw.githubusercontent.com/azureblueday/starscripts/refs/heads/main/flagfb-obfuscated.lua", -- flag fb
+    [4864117649] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebb01m0001ffa5g5590gfg", -- utg
+    [6331902150] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebc6wa0003ffa57d8vnhw4", -- forsaken
+    [8558141897] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebcr4t0005ffa5lxdsbfga", -- flag fb
     [3150475059] = "https://raw.githubusercontent.com/azureblueday/starscripts/refs/heads/main/ff2.lua", -- ff2
 }
 
