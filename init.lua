6dfd5cdb037ff0a928f6ed5c54801eedb28605ce
azureblueday <gamerspace32@gmail.com>
2026-02-23 21:13:59 -0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local Scripts = {
     [4864117649] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebb01m0001ffa5g5590gfg", -- utg
     [6331902150] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebc6wa0003ffa57d8vnhw4", -- forsaken
     [8558141897] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmlebcr4t0005ffa5lxdsbfga", -- flag fb
-    [3150475059] = "https://raw.githubusercontent.com/azureblueday/starscripts/refs/heads/main/ff2.lua", -- ff2
+    [3150475059] = "https://bastiondev.org/api/loader/cmle54vwo0001fam85t16u67z?script=cmm05gshm000d10pmkkh6fbw4", -- ff2
 }
 
 local Script = Scripts[game.GameId]
